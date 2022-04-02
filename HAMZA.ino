9ad863955b588d764064edd1d4dd3ecfb8bca4bb
Muhammad hamza <102409025+muhammadhamza890@users.noreply.github.com>
2022-04-02 13:46:36 +0500

    Arduino Basic circuit with code:

---
+++ HAMZA.ino
@@ -0,0 +1,55 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(9, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(13, HIGH);
+  delay(200); // Wait for 1000 millisecond(s)
+  digitalWrite(13, LOW);
+  delay(200); // Wait for 1000 millisecond(s)
+  //
+  digitalWrite(12, HIGH);
+  delay(200); // Wait for 1000 millisecond(s)
+  digitalWrite(12, LOW);
+  delay(200); // Wait for 1000 millisecond(s)
+  //
+  digitalWrite(11, HIGH);
+  delay(200); // Wait for 1000 millisecond(s)
+  digitalWrite(11, LOW);
+  delay(200); // Wait for 1000 millisecond(s)
+  //
+  digitalWrite(10, HIGH);
+  delay(200); // Wait for 1000 millisecond(s)
+  digitalWrite(10, LOW);
+  delay(200); // Wait for 1000 millisecond(s)
+  //
+  digitalWrite(9, HIGH);
+  delay(200); // Wait for 1000 millisecond(s)
+  digitalWrite(9, LOW);
+  delay(200); // Wait for 1000 millisecond(s)
+  //revers
+  
+  //
+   digitalWrite(10,HIGH);
+  delay(200); // Wait for 1000 millisecond(s)
+  digitalWrite(10,LOW);
+  delay(200); // Wait for 1000 millisecond(s)
+  //
+  digitalWrite(11,HIGH);
+  delay(200); // Wait for 1000 millisecond(s)
+  digitalWrite(11,LOW);
+  delay(200); // Wait for 1000 millisecond(s)
+  //
+  digitalWrite(12,HIGH);
+  delay(200); // Wait for 1000 millisecond(s)
+  digitalWrite(12,LOW);
+  delay(200); // Wait for 1000 millisecond(s)
+}
